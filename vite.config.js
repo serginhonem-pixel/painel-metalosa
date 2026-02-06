e89014bac59749d938e7ebe95f30d6f72c6b5f80
Sergio <sergiobetinim@gmail.com>
2026-02-06 07:51:19 -0300

    Foi

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const appConfigPlugin = () => {
+  const getContent = () => {
+    const key =
+      process.env.FIREBASE_VAPID_KEY ||
+      process.env.VITE_FIREBASE_VAPID_KEY ||
+      ''
+    const payload = { FIREBASE_VAPID_KEY: key }
+    return `window.__APP_CONFIG__ = ${JSON.stringify(payload)};\n`
+  }
+
+  return {
+    name: 'app-config',
+    configureServer(server) {
+      server.middlewares.use('/app-config.js', (req, res) => {
+        res.setHeader('Content-Type', 'application/javascript')
+        res.end(getContent())
+      })
+    },
+    generateBundle() {
+      this.emitFile({
+        type: 'asset',
+        fileName: 'app-config.js',
+        source: getContent(),
+      })
+    },
+  }
+}
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), appConfigPlugin()],
   resolve: {
     dedupe: ['react', 'react-dom'],
   },
